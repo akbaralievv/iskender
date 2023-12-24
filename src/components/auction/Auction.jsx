ddab95670bcdbaaf1cac4445380bd7ac08db21da
AkbaralievAlisher <kgkgg28@gmail.com>
2023-12-24 18:13:40 +0600

    fix

--- src/components/auction/Auction.jsx
+++ src/components/auction/Auction.jsx
@@ -10,51 +10,50 @@ import dec from '../../assets/icons/minus.svg';
 import heart from '../../assets/icons/heart.svg';
 import bestsellers from '../../assets/images/bestsellers.png';
 
-const PrevArrowIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-    <path
-      d="M18.8125 24.9L10.6625 16.75C9.7 15.7875 9.7 14.2125 10.6625 13.25L18.8125 5.10001"
-      stroke="#171717"
-      strokeWidth="1.5"
-      strokeMiterlimit="10"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
-const NextArrowIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-    <path
-      d="M11.1875 5.09999L19.3375 13.25C20.3 14.2125 20.3 15.7875 19.3375 16.75L11.1875 24.9"
-      stroke="#171717"
-      strokeWidth="1.5"
-      strokeMiterlimit="10"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
-function CustomPrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div className={className} style={{ ...style, display: 'block' }} onClick={onClick}>
-      <PrevArrowIcon />
-    </div>
-  );
-}
-
-function CustomNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div className={className} style={{ ...style, display: 'block' }} onClick={onClick}>
-      <NextArrowIcon />
-    </div>
-  );
-}
-
 const Auction = () => {
+  const PrevArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+      <path
+        d="M18.8125 24.9L10.6625 16.75C9.7 15.7875 9.7 14.2125 10.6625 13.25L18.8125 5.10001"
+        stroke="#171717"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+
+  const NextArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+      <path
+        d="M11.1875 5.09999L19.3375 13.25C20.3 14.2125 20.3 15.7875 19.3375 16.75L11.1875 24.9"
+        stroke="#171717"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+
+  function CustomPrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className={className} style={{ ...style, display: 'block' }} onClick={onClick}>
+        <PrevArrowIcon />
+      </div>
+    );
+  }
+
+  function CustomNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className={className} style={{ ...style, display: 'block' }} onClick={onClick}>
+        <NextArrowIcon />
+      </div>
+    );
+  }
   const settings = {
     dots: true,
     infinite: true,
