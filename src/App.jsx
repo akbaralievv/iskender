6b73a69c38bb2608250ea01d50829cb5313e0e01
AkbaralievAlisher <kgkgg28@gmail.com>
2023-12-24 17:14:53 +0600

    fix: change import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/header/Header';
 import Current from './components/current/Current';
 import Categories from './components/categories/Categories';
 import Bestsellers from './components/bestsellers/Bestsellers';
-import News from './components/news/News';
+import News from './components/news/News.jsx';
 import Auction from './components/auction/auction';
 import Addresses from './components/addresses/Addresses';
 import Footer from './components/footer/Footer';
